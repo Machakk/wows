26e77979d43ea68f0f84b55f8bb23ac2005e8d02
sawsanalkebsi <sawsan.alkebsi.com>
2021-06-15 22:43:01 +0200

    vue

--- wows/resources/js/app.js
+++ wows/resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from "vue"
+
+import AppCal from './components/App.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: { AppCal }
+});
---
+++ wows/resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{message}}
+  </div>
+</template>
+<script>
+const default_layout = "default";
+
+
+export default {
+  computed: {},
+  name : "AppCal",
+  data() {
+      return {
+          message:'Hello Sawsannn :)'
+      }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
